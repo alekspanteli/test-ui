3613451ed98f393e4d1ceb4bdcdbfeadaf4eec1f
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2024-08-01 11:29:16 +0000

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <h1 className="mt-5 p-5 text-3xl font-bold underline">Hello world!!!</h1>
+    <h1 className="mt-5 p-5 text-3xl font-bold underline">Hello world!!!!!</h1>
   )
 }
